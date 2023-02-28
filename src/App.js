3950b3583502a6a41cea89c655c172cdafaa8a1c
MaxenceAll <maxence.allart@gmail.com>
2023-02-28 22:44:52 +0100

    noteapp finished

--- src/App.js
+++ src/App.js
@@ -41,22 +41,13 @@ export default function App() {
             return newArray
         })
     }
-    
-    /**
-     * Challenge: complete and implement the deleteNote function
-     * 
-     * Hints: 
-     * 1. What array method can be used to return a new
-     *    array that has filtered out an item based 
-     *    on a condition?
-     * 2. Notice the parameters being based to the function
-     *    and think about how both of those parameters
-     *    can be passed in during the onClick event handler
-     */
+
     
     function deleteNote(event, noteId) {
         event.stopPropagation()
         console.log("deleted note " , noteId)
+        setNotes( prevNotes => prevNotes.filter( (note)=> note.id !== noteId ))
+
     }
     
     function findCurrentNote() {
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,6 @@ export default function Sidebar(props) {
     const noteElements = props.notes.map((note, index) => (
         <div key={note.id}>
             <div
-                
                 className={`title ${
                     note.id === props.currentNote.id ? "selected-note" : ""
                 }`}
@@ -13,8 +12,7 @@ export default function Sidebar(props) {
                 <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
                 <button 
                     className="delete-btn"
-                    // Your onClick event handler here
-                    onClick= { ()=> props.deleteNote(event, note.id)}
+                    onClick={(event) => props.deleteNote(event, note.id)}
                 >
                     <i className="gg-trash trash-icon"></i>
                 </button>
